d2d882c1ec807c1e18815a9298fe17a568c80449
isa-er <err.isa99@gmail.com>
2025-04-22 14:17:52 +0300

    ...

--- SignalRWebUI/Controllers/BasketsController.cs
+++ SignalRWebUI/Controllers/BasketsController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using Newtonsoft.Json;
 using SignalRWebUI.Dtos.BasketDtos;
 using System.Text;
@@ -19,10 +20,13 @@ namespace SignalRWebUI.Controllers
 
 
 
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(int id)
         {
+            TempData["tableId"] = id;
+
+
             var client = _httpClientFactory.CreateClient(); 
-            var responseMessage = await client.GetAsync("https://localhost:7113/api/Basket/BasketListByMenuTableWithProductName?id=1"); 
+            var responseMessage = await client.GetAsync("https://localhost:7113/api/Basket/BasketListByMenuTableWithProductName?id="+id); 
 
             if (responseMessage.IsSuccessStatusCode) // başarılı ise
             {
@@ -38,12 +42,14 @@ namespace SignalRWebUI.Controllers
 
         public async Task<IActionResult> DeleteBasket(int id)
         {
+            int tableId = int.Parse(TempData["tableId"].ToString());
+
             var client = _httpClientFactory.CreateClient();
             var responseMessage = await client.DeleteAsync($"https://localhost:7113/api/Basket/{id}");
 
             if (responseMessage.IsSuccessStatusCode)
             {
-                return RedirectToAction("Index");
+                return RedirectToAction("Index",new {id=tableId});
             }
             return NoContent();
         }
@@ -53,12 +59,15 @@ namespace SignalRWebUI.Controllers
 
         public async Task<IActionResult> IncreaseProductCount(int id)
         {
+            int tableId = int.Parse(TempData["tableId"].ToString());
+
+
             var client = _httpClientFactory.CreateClient();
             var responseMessage = await client.PutAsync($"https://localhost:7113/api/Basket/increase/{id}",null);
 
             if (responseMessage.IsSuccessStatusCode)
             {
-                return RedirectToAction("Index");
+                return RedirectToAction("Index", new { id = tableId });
             }
 
             return NoContent();
@@ -66,12 +75,14 @@ namespace SignalRWebUI.Controllers
 
         public async Task<IActionResult> DecreaseProductCount(int id)
         {
+            int tableId = int.Parse(TempData["tableId"].ToString());
+
             var client = _httpClientFactory.CreateClient();
             var responseMessage = await client.PutAsync($"https://localhost:7113/api/Basket/decrease/{id}",null);
 
             if (responseMessage.IsSuccessStatusCode)
             {
-                return RedirectToAction("Index");
+                return RedirectToAction("Index", new { id = tableId });
             }
 
             return NoContent();
---
+++ SignalRapi/Mapping/NotificationMapping.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using SignalR.DtoLayer.NotificationDto;
+using SignalR.EntityLayer.Entities;
+
+namespace SignalRapi.Mapping
+{
+    public class NotificationMapping:Profile
+    {
+        public NotificationMapping()
+        {
+            
+            CreateMap<Notification, ResultNotificationDto>().ReverseMap();
+            CreateMap<Notification, CreateNotificationDto>().ReverseMap();
+            CreateMap<Notification, UpdateNotificationDto>().ReverseMap();
+            CreateMap<Notification, GetByIdNotificationDto>().ReverseMap();
+        }
+    }
+}
